39bf2fe25b87585f50adbf7df79ef2121ea0a74a
misalitvin <misalitvin100@gmail.com>
2024-10-03 19:30:32 +0200

    JavaScript connected to Controller.

--- target/classes/static/js/monsterConnect.js
+++ target/classes/static/js/monsterConnect.js
@@ -1,26 +1,3 @@
-// monsterConnect.js
-
-// Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', () => {
-    const submitButton = document.getElementById('SubmitButton');
-
-    // Add an event listener for the click event
-    submitButton.addEventListener('click', () => {
-        onSubmitButtonClick(); // Call your function when the button is clicked
-    });
-});
-
-// Function to handle the button click
-function onSubmitButtonClick() {
-    const nameToSend = "Nigro"; // The word you want to send
-
-    // Send the word to the Java backend
-    fetch('http://localhost:8080/hello', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/plain', // Set content type to plain text
-        },
-        body: nameToSend,// Send the plain string directly
-    })
-
+SubmitButton.onclick = function(){
+    console.log("I am working");
 }
